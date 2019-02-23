e96a6283b743348cf07e84bc75bb9d1f02da5fbd
devops4j <devops4j@devops4j.com>
2019-02-23 22:42:56 +0800

    修改版权声明

--- interface-platform-ipdb/src/main/java/com/rnkrsoft/platform/ipdb/Database.java
+++ interface-platform-ipdb/src/main/java/com/rnkrsoft/platform/ipdb/Database.java
@@ -339,17 +339,80 @@
  * library.  If this is what you want to do, use the GNU Lesser General
  * Public License instead of this License.
  */
-package com.rnkrsoft.platform.service;
+package com.rnkrsoft.platform.ipdb;
 
-import com.rnkrsoft.platform.InterfaceContext;
+import com.rnkrsoft.platform.ipdb.exception.IPFormatException;
+import com.rnkrsoft.platform.ipdb.exception.InvalidDatabaseException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
- * Created by rnkrsoft.com on 2018/6/25.
+ * Created by woate on 2018/12/2.
  */
-public interface InterfaceDefinitionService {
-    /**
-     * 设置接口定义信息
-     * @param context 对象定义对象
-     */
-    boolean setting(InterfaceContext context);
+public class Database {
+    private Reader reader;
+
+    public Database(URL in) throws IOException, URISyntaxException {
+        this.reader = new Reader(in);
+    }
+
+    public boolean reload(URL in) {
+        try {
+            Reader r = new Reader(in);
+            this.reader = r;
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public BaseStation findBaseStation(String addr, String language) throws IPFormatException, InvalidDatabaseException {
+        String[] data = this.reader.find(addr, language);
+        if (data == null) {
+            return null;
+        }
+        return new BaseStation(this.reader.getSupportFields(), data);
+    }
+
+    public City findCity(String addr, String language) throws IPFormatException, InvalidDatabaseException {
+
+        String[] data = this.reader.find(addr, language);
+        if (data == null) {
+            return null;
+        }
+        return new City( this.reader.getSupportFields(), data);
+    }
+
+    public District findDistrict(String addr, String language) throws IPFormatException, InvalidDatabaseException {
+        String[] data = this.reader.find(addr, language);
+        if (data == null) {
+            return null;
+        }
+
+        return new District(this.reader.getSupportFields(),data);
+    }
+
+    public IDC findIdc(String addr, String language) throws IPFormatException, InvalidDatabaseException {
+        String[] data = this.reader.find(addr, language);
+        if (data == null) {
+            return null;
+        }
+        return new IDC(this.reader.getSupportFields(), data);
+    }
+
+
+    public boolean isIPv4() {
+        return (this.reader.getMeta().ipVersion & 0x01) == 0x01;
+    }
+
+    public boolean isIPv6() {
+        return (this.reader.getMeta().ipVersion & 0x02) == 0x02;
+    }
+
+    public int buildTime() {
+        return this.reader.getBuildUTCTime();
+    }
 }
--- interface-platform-web/src/main/java/com/rnkrsoft/platform/configure/web/servlet/ConfigureServlet.java
+++
@@ -1,9 +0,0 @@
-package com.rnkrsoft.platform.configure.web.servlet;
-
-import javax.servlet.http.HttpServlet;
-
-/**
- * Created by rnkrsoft.com on 2018/10/14.
- */
-public class ConfigureServlet extends HttpServlet{
-}
